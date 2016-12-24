493e9cfa70d2926b2cbd49a423ac97105c24508c
Rodrigo Recoba <Rodrigo33024@gmail.com>
2016-12-24 14:25:29 -0600

    Cleans up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :contacts
-  get 'about', to: 'pages#about'
   root to: 'pages#index'
-  get 'contact-us', to: 'contacts#new'
+  get 'about', to: 'pages#about'
+  resources :contacts, only: [:create]
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
