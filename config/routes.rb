2d3753b8095d700d179072fe6cc548ddc56f33d0
Rodrigo Recoba <Rodrigo33024@gmail.com>
2016-12-24 14:09:09 -0600

    Updates contact us route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :contacts
   get 'about', to: 'pages#about'
   root to: 'pages#index'
+  get 'contact-us', to: 'contacts#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
